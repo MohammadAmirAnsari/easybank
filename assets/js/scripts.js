dec9d525adfabd51723aefb21590b5869e61164f
Mohammad Amir Ansari <amiransari1721@gmail.com>
2026-02-07 03:05:53 +0530

    add next card in mobile complete header and overlay

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,15 @@
 let hamburger = document.querySelector('.header__hamburger')
 let overlay = document.querySelector('.overlay')
 
-hamburger.addEventListener('click',()=>{
+hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('open')
     overlay.classList.toggle('overlay--active')
+
+    if (overlay.classList.contains('overlay--active')) {
+        document.body.classList.add('no-scroll')
+    }else{
+        document.body.classList.remove('no-scroll')
+
+    }
 })
+// https://qxjz61vq-3000.inc1.devtunnels.ms/
