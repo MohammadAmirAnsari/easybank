1a17b52c142f407d381a4dd61856c7795e11e972
Mohammad Amir Ansari <amiransari1721@gmail.com>
2026-02-13 01:28:46 +0530

    refactor header with single ul

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,15 +1,22 @@
 let hamburger = document.querySelector('.header__hamburger')
 let overlay = document.querySelector('.overlay')
+let menu = document.querySelector('header ul')
 
 hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('open')
+    hamburger.classList.toggle('hamburger--open')
     overlay.classList.toggle('overlay--active')
-
+    menu.classList.toggle('menu--open')
+    
     if (overlay.classList.contains('overlay--active')) {
         document.body.classList.add('no-scroll')
     }else{
         document.body.classList.remove('no-scroll')
-
     }
+
+    // if (menu.classList.contains('open')) {
+    //     menu.classList.add('no-scroll')
+    // }else{
+    //     menu.classList.remove('no-scroll')
+    // }
 })
 // https://qxjz61vq-3000.inc1.devtunnels.ms/
